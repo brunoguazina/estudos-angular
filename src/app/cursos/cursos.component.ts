50759916e6500cfbeeb8981bcecdce5aed40a641
brunoguazina <bruno@guazina.com>
2017-12-25 22:28:22 -0300

    migrando projeto para scss

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -12,12 +12,18 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent {
 
   cursos: string [];
-  
+  valorAtual = '';
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log();
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
 
   constructor(private cursosService: CursosService) {
 
       this.cursos = this.cursosService.getCursos();
-
+      
   }
+  
   
 }
